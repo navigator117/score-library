a3ee58eae45290eb5c577bf0a517d91004ed0828
Michael Elsdörfer <michael@elsdoerfer.com>
2013-08-28 20:10:03 +0200

    Build: Consolidate debug mode, change output name.

--- build-scorediv.sh
+++ build-scorediv.sh
@@ -1,16 +1,21 @@
 #! /bin/bash
 
-./public/closure-library/closure/bin/build/closurebuilder.py \
+if [ "$1" = "--debug" ]; then
+    extra_args='--compiler_flags="--formatting=PRETTY_PRINT" --compiler_flags="--debug"'
+fi
+
+eval ./public/closure-library/closure/bin/build/closurebuilder.py \
 --root='./public/closure-library/' \
 --root='./src' \
---namespace="ScoreLibrary" \
+--input='./src/scorelibrary.js' \
 --output_mode=compiled \
 --compiler_jar=./support/compiler.jar \
---compiler_flags="--compilation_level=ADVANCED_OPTIMIZATIONS" \
---compiler_flags="--create_source_map=./scorediv-compiled.map" \
+--compiler_flags="--compilation_level=WHITESPACE_ONLY" \
+--compiler_flags="--create_source_map=./scorelib.map" \
 --compiler_flags="--generate_exports" \
 --compiler_flags="--externs=./support/externs/html5.js" \
 --compiler_flags="--externs=./support/externs/fileapi.js" \
 --compiler_flags="--externs=./support/externs/contrib/jquery-1.7.js" \
 --compiler_flags="--externs=./support/externs/contrib/zip.js" \
---compiler_flags="--js_output_file=./scorediv-compiled.js"
+--compiler_flags="--js_output_file=./scorelib.min.js" \
+$extra_args
