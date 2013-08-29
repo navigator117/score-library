52bd66a2c08a76ecd54981e407939773019278a1
Michael Elsdörfer <michael@elsdoerfer.com>
2013-08-29 05:35:23 +0200

    Integrate reduce-license.js into build script.

--- build-scorediv.sh
+++ build-scorediv.sh
@@ -17,11 +17,35 @@ find ./src -name "*.js" | grep -v ./src/renderer/fonts \
         --output_mode=compiled \
         --compiler_jar=./support/compiler.jar \
         --compiler_flags="--compilation_level=ADVANCED_OPTIMIZATIONS" \
-        --compiler_flags="--create_source_map=./scorelib.map" \
         --compiler_flags="--generate_exports" \
         --compiler_flags="--externs=./support/externs/html5.js" \
         --compiler_flags="--externs=./support/externs/fileapi.js" \
         --compiler_flags="--externs=./support/externs/contrib/jquery-1.7.js" \
         --compiler_flags="--externs=./support/externs/contrib/zip.js" \
-        --compiler_flags="--js_output_file=./scorelib.min.js" \
-        "${extra_args[@]}"
+        "${extra_args[@]}" \
+    | java -jar ./support/compiler.jar \
+        --compilation_level WHITESPACE_ONLY  \
+        --create_source_map ./scorelib.map  \
+        --js_output_file ./scorelib.min.js
+        # Running closure again is the only way to make it remove comments
+        # that have a @license instruction in them.
+
+echo "/*
+ This file is part of
+ score-library <http://www.musicxml-viewer.com>.
+ author & contact: XiongWenjie (navigator117 at gmail.com)
+ score-library is free software:
+ you can redistribute it and/or modify it under the terms of the
+ GNU General Public License as published by the Free Software Foundation,
+ either version 3 of the License, or (at your option) any later version.
+
+ score-library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with score-library.
+ If not, see <http://www.gnu.org/licenses>.
+*/
+" >> scorelib.min.js
